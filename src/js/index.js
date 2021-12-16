b415ae9badbfb7dfc2bdbd5a595b140a5054cd68
CRodr86 <rodrigocfotografia@gmail.com>
2021-12-16 11:03:43 +0000

    deleted blank lines

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-
 import "../styles/index.scss";
 
-
 import Layout from "./layout";
-
 
 ReactDOM.render(<Layout />, document.querySelector("#app"));
